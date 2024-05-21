5609f1d3248cd6858a8b3209b473702c86d8e3d3
Luis Naal Pacheco <lucasnaal8@gmail.com>
2024-05-21 14:02:01 -0600

    correcciones ortograficas

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "./database/connection";
 
 app.listen(app.get("port"));
 
-console.log("Server escuando en el puerto", app.get("port"));
+console.log("Server escuchando en el puerto", app.get("port"));
